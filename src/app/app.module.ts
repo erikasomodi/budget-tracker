076ea14d568e6295c48081a7708496b81e21622b
erikasomodi <meszesanerika@gmail.com>
2024-07-15 23:42:31 +0200

    nav component létrehozása és bekötése

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [
     provideFirebaseApp(() =>
