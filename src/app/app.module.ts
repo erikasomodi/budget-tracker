24ae9ad36616e0b80cd0031727a8043be479732e
erikasomodi <meszesanerika@gmail.com>
2024-07-15 23:27:25 +0200

    projekt kezdet - app html indítása

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
-import { getAuth, provideAuth } from "@angular/fire/auth";
-import { getFirestore, provideFirestore } from "@angular/fire/firestore";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,13 +13,13 @@ import { getFirestore, provideFirestore } from "@angular/fire/firestore";
   providers: [
     provideFirebaseApp(() =>
       initializeApp({
-        apiKey: "AIzaSyAc9vgYmywgGJHWefTdt2KPuIwsfkRAHTU",
-        authDomain: "angular-firebase-project-alpha.firebaseapp.com",
-        projectId: "angular-firebase-project-alpha",
-        storageBucket: "angular-firebase-project-alpha.appspot.com",
-        messagingSenderId: "552703926716",
-        appId: "1:552703926716:web:981086f1b6e2b319ae0bb9",
-        measurementId: "G-NHCKBRYHJF",
+        apiKey: 'AIzaSyAc9vgYmywgGJHWefTdt2KPuIwsfkRAHTU',
+        authDomain: 'angular-firebase-project-alpha.firebaseapp.com',
+        projectId: 'angular-firebase-project-alpha',
+        storageBucket: 'angular-firebase-project-alpha.appspot.com',
+        messagingSenderId: '552703926716',
+        appId: '1:552703926716:web:981086f1b6e2b319ae0bb9',
+        measurementId: 'G-NHCKBRYHJF',
       })
     ),
     provideAuth(() => getAuth()),
