e12c75a125bc2dbbbc4e125b0092eed5a0d88411
erikasomodi <meszesanerika@gmail.com>
2024-08-02 20:48:43 +0200

    szerkezet rendbetétele: 1. toastr az app modulba importálva, hogy működjön a transaction reg 2. transactions komponens törlése, mert már van transactionünk, amin belül létezik a  reg és a list 3. margin, padding topok, hogy minden látszódjon 4. registration és user-reg még mindig létezik duplán, azt majd el kell dönteni hogy melyik legyen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
-import { getAuth, provideAuth } from "@angular/fire/auth";
-import { getFirestore, provideFirestore } from "@angular/fire/firestore";
-import { NavComponent } from "./components/nav/nav.component";
-import { HomePageComponent } from "./components/home-page/home-page.component";
-import { RegistrationComponent } from "./components/registration/registration.component";
-import { TransactionsComponent } from "./components/transactions/transactions.component";
-import { BudgetTrackerComponent } from "./components/budget-tracker/budget-tracker.component";
-import { LoginComponent } from "./components/login/login.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { NavComponent } from './components/nav/nav.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { BudgetTrackerComponent } from './components/budget-tracker/budget-tracker.component';
+import { LoginComponent } from './components/login/login.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -19,21 +19,20 @@ import { LoginComponent } from "./components/login/login.component";
     NavComponent,
     HomePageComponent,
     RegistrationComponent,
-    TransactionsComponent,
     BudgetTrackerComponent,
     LoginComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ToastrModule.forRoot()],
   providers: [
     provideFirebaseApp(() =>
       initializeApp({
-        apiKey: "AIzaSyAc9vgYmywgGJHWefTdt2KPuIwsfkRAHTU",
-        authDomain: "angular-firebase-project-alpha.firebaseapp.com",
-        projectId: "angular-firebase-project-alpha",
-        storageBucket: "angular-firebase-project-alpha.appspot.com",
-        messagingSenderId: "552703926716",
-        appId: "1:552703926716:web:981086f1b6e2b319ae0bb9",
-        measurementId: "G-NHCKBRYHJF",
+        apiKey: 'AIzaSyAc9vgYmywgGJHWefTdt2KPuIwsfkRAHTU',
+        authDomain: 'angular-firebase-project-alpha.firebaseapp.com',
+        projectId: 'angular-firebase-project-alpha',
+        storageBucket: 'angular-firebase-project-alpha.appspot.com',
+        messagingSenderId: '552703926716',
+        appId: '1:552703926716:web:981086f1b6e2b319ae0bb9',
+        measurementId: 'G-NHCKBRYHJF',
       })
     ),
     provideAuth(() => getAuth()),
