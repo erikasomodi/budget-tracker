3c6048a51a9eba3d666cc3c4fb29e8e85668d1c6
Kázsmér Anita (Panni) <you@example.compannikazsmer@gmail.com>
2024-08-09 18:08:40 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { ReactiveFormsModule } from "@angular/forms";
 import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
 import { getAuth, provideAuth } from "@angular/fire/auth";
 import { getFirestore, provideFirestore } from "@angular/fire/firestore";
@@ -23,7 +24,7 @@ import { LoginComponent } from "./components/login/login.component";
     BudgetTrackerComponent,
     LoginComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [
     provideFirebaseApp(() =>
       initializeApp({
