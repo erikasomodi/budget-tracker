0150ed30f5be8eb21075eabc3207942cf389cc83
erikasomodi <meszesanerika@gmail.com>
2024-08-12 10:53:28 +0200

    budegt tracker oldal kezdetek + fontawesome telepítése (npm i majd lehet szükséges)

--- src/app/components/budget-tracker/budget-tracker.component.ts
+++ src/app/components/budget-tracker/budget-tracker.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import {
+  faShirt,
+  faGift,
+  faPizzaSlice,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-budget-tracker',
   templateUrl: './budget-tracker.component.html',
-  styleUrl: './budget-tracker.component.scss'
+  styleUrl: './budget-tracker.component.scss',
 })
 export class BudgetTrackerComponent {
-
+  faShirt = faShirt;
+  faGift = faGift;
+  faPizzaSlice = faPizzaSlice;
+  expenses = [
+    {
+      category: 'Clothes',
+      amount: 498.5,
+      percentage: 32,
+      icon: this.faShirt,
+    },
+    {
+      category: 'Gifts',
+      amount: 344.45,
+      percentage: 21,
+      icon: this.faGift,
+    },
+    {
+      category: 'Food',
+      amount: 230.5,
+      percentage: 12,
+      icon: this.faPizzaSlice,
+    },
+    // Add more expenses as necessary
+  ];
 }
