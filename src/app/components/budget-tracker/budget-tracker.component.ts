bc2e1e6fc96adddf651af7ca880aba46c40b9342
erikasomodi <meszesanerika@gmail.com>
2024-08-23 23:00:57 +0200

    expense, income tranzakciótípus bevezetése, hogy el tudjam különíteni a tranzakciókat; kérdőjeles, mert a tranzakció rögzítésben nincs benne és ne rontsa el;

--- src/app/components/budget-tracker/budget-tracker.component.ts
+++ src/app/components/budget-tracker/budget-tracker.component.ts
@@ -9,14 +9,17 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { TransactionModel } from '../../models/transaction.model';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { UserModel } from '../../models/user.model';
 
 @Component({
   selector: 'app-budget-tracker',
   templateUrl: './budget-tracker.component.html',
-  styleUrl: './budget-tracker.component.scss',
+  styleUrls: ['./budget-tracker.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BudgetTrackerComponent implements OnInit {
+  user: UserModel;
+
   // expense icons
   faShirt: IconProp = faShirt;
   faGift: IconProp = faGift;
@@ -26,11 +29,14 @@ export class BudgetTrackerComponent implements OnInit {
   faLaptop: IconProp = faLaptop;
   faChartLine: IconProp = faChartLine;
 
+  // a jelenlegi összeg, amit a felhasználó lát a kiválasztott nézet szerint
   currentSum: number = 0;
 
+  // kiadások
   expenses: TransactionModel[] = [
     {
-      transactionName: 'Clothes',
+      transactionName: 'Őszi cipő',
+      transactionType: 'expense',
       transactionAmount: 24985,
       transactionDate: '2024-08-01',
       transactionCategory: 'Shopping',
@@ -38,7 +44,17 @@ export class BudgetTrackerComponent implements OnInit {
       icon: this.faShirt,
     },
     {
+      transactionName: 'Futó nadrág',
+      transactionType: 'expense',
+      transactionAmount: 13435,
+      transactionDate: '2024-08-01',
+      transactionCategory: 'Shopping',
+      transactionMethod: 'Bank Transfer',
+      icon: this.faShirt,
+    },
+    {
       transactionName: 'Book',
+      transactionType: 'expense',
       transactionAmount: 4985,
       transactionDate: '2024-08-03',
       transactionCategory: 'Gifts',
@@ -47,16 +63,37 @@ export class BudgetTrackerComponent implements OnInit {
     },
     {
       transactionName: 'Pizza',
+      transactionType: 'expense',
       transactionAmount: 5000,
       transactionDate: '2024-08-05',
       transactionCategory: 'Food',
       transactionMethod: 'Card',
       icon: this.faPizzaSlice,
     },
+    {
+      transactionName: 'Piac',
+      transactionType: 'expense',
+      transactionAmount: 15000,
+      transactionDate: '2024-08-23',
+      transactionCategory: 'Food',
+      transactionMethod: 'Card',
+      icon: this.faPizzaSlice,
+    },
+    {
+      transactionName: 'Lidl',
+      transactionType: 'expense',
+      transactionAmount: 167327,
+      transactionDate: '2024-08-23',
+      transactionCategory: 'Food',
+      transactionMethod: 'Card',
+      icon: this.faPizzaSlice,
+    },
   ];
+  // bevételek
   incomes: TransactionModel[] = [
     {
       transactionName: 'Fizu',
+      transactionType: 'income',
       transactionAmount: 450000,
       transactionDate: '2024-08-01',
       transactionCategory: 'Income',
@@ -65,6 +102,7 @@ export class BudgetTrackerComponent implements OnInit {
     },
     {
       transactionName: 'Családi pótlék',
+      transactionType: 'income',
       transactionAmount: 25000,
       transactionDate: '2024-08-03',
       transactionCategory: 'FreeLance',
@@ -72,75 +110,153 @@ export class BudgetTrackerComponent implements OnInit {
       icon: this.faMoneyBill,
     },
     {
+      transactionName: 'Adó visszatérítés',
+      transactionType: 'income',
+      transactionAmount: 93272,
+      transactionDate: '2024-08-03',
+      transactionCategory: 'FreeLance',
+      transactionMethod: 'Bank Transfer',
+      icon: this.faMoneyBill,
+    },
+    {
       transactionName: 'Tőzsde',
+      transactionType: 'income',
       transactionAmount: 5000,
       transactionDate: '2024-08-10',
       transactionCategory: 'Investments',
       transactionMethod: 'Bank Transfer',
       icon: this.faChartLine,
     },
+    {
+      transactionName: 'Családi pótlék',
+      transactionType: 'income',
+      transactionAmount: 25000,
+      transactionDate: '2024-08-23',
+      transactionCategory: 'FreeLance',
+      transactionMethod: 'Bank Transfer',
+      icon: this.faMoneyBill,
+    },
+    {
+      transactionName: 'Tőzsde',
+      transactionType: 'income',
+      transactionAmount: 5000,
+      transactionDate: '2024-08-23',
+      transactionCategory: 'Investments',
+      transactionMethod: 'Bank Transfer',
+      icon: this.faChartLine,
+    },
   ];
 
-  // transactions: TransactionModel[] = [...this.expenses, ...this.incomes];
-  transactions: TransactionModel[] = [
-    ...this.expenses.map((transaction) => ({
+  constructor() {
+    // Felhasználó létrehozása
+    this.user = {
+      id: '1',
+      name: 'Somodi Era',
+      username: 'SomodiEra',
+      password: 'PROGbudget2024',
+      age: 40,
+      married: true,
+      numberOfChildren: 2,
+      startBudget: 1000000,
+      monthlySalary: 400000,
+      transactions: [...this.expenses, ...this.incomes],
+    };
+  }
+  // A kiadások negatív előjellel, hogy az összegzés helyes legyen
+  setExpensesNegative(transaction: TransactionModel): TransactionModel {
+    return {
       ...transaction,
-      transactionAmount: -Math.abs(transaction.transactionAmount), // Negatív lesz
-    })),
+      transactionAmount: -Math.abs(transaction.transactionAmount),
+    };
+  }
+
+  // Az összes tranzakció, a kiadások negatív előjellel
+  transactions: TransactionModel[] = [
+    ...this.expenses.map((transaction) =>
+      this.setExpensesNegative(transaction)
+    ),
     ...this.incomes,
   ];
 
-  transactionsSum: number = this.transactions.reduce(
-    (sum, transaction) => sum + transaction.transactionAmount,
-    0
-  );
+  // Az összegzés a startBudget hozzáadásával, ha a transactions nézet van kiválasztva
+  get transactionSums(): number {
+    const expensesTotal = this.calculateSum('expenses');
+    const incomesTotal = this.calculateSum('incomes');
+    return this.user.startBudget - expensesTotal + incomesTotal;
+  }
 
-  currentView: 'expenses' | 'incomes' | 'transactions' = 'expenses';
+  // Az aktuális nézet
+  currentView: 'expenses' | 'incomes' | 'transactions' = 'transactions';
 
+  // Nézet váltás és az összegzés kiszámítása
   switchView(view: 'expenses' | 'incomes' | 'transactions') {
     this.currentView = view;
     this.currentSum = this.calculateSum(view);
   }
 
+  // Az összegzés kiszámítása a kiválasztott nézet szerint
   calculateSum(view: 'expenses' | 'incomes' | 'transactions'): number {
     let transactions;
+    // A kiadások, bevételek és az összes tranzakció kiválasztása a nézet szerint
     if (view === 'expenses') {
-      transactions = this.expenses;
+      transactions = this.user.transactions
+        .filter(
+          (transaction) =>
+            transaction.transactionCategory === 'Shopping' ||
+            transaction.transactionCategory === 'Gifts' ||
+            transaction.transactionCategory === 'Food'
+        )
+        .map((transaction) => this.setExpensesNegative(transaction));
+      // A bevételek kiválasztása a nézet szerint
     } else if (view === 'incomes') {
-      transactions = this.incomes;
+      transactions = this.user.transactions.filter(
+        (transaction) =>
+          transaction.transactionCategory === 'Income' ||
+          transaction.transactionCategory === 'FreeLance' ||
+          transaction.transactionCategory === 'Investments'
+      );
+      // Az összes tranzakció kiválasztása
     } else {
-      transactions = this.transactions;
+      transactions = this.user.transactions.map((transaction) => {
+        if (
+          transaction.transactionCategory === 'Shopping' ||
+          transaction.transactionCategory === 'Gifts' ||
+          transaction.transactionCategory === 'Food'
+        ) {
+          return this.setExpensesNegative(transaction);
+        }
+        return transaction;
+      });
     }
 
-    return transactions.reduce(
+    // Az összegzés kiszámítása a kiválasztott nézet szerint
+    const sum = transactions.reduce(
       (sum, transaction) => sum + transaction.transactionAmount,
       0
     );
-  }
 
-  months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
+    return view === 'transactions' ? this.user.startBudget + sum : sum; // startBudget hozzáadása a transactions nézethez
+  }
 
   ngOnInit() {
     // Negatív lesz az összeg
-    this.expenses = this.expenses.map((transaction) => ({
-      ...transaction,
-      transactionAmount: -Math.abs(transaction.transactionAmount),
-    }));
+    this.expenses = this.expenses.map((transaction) =>
+      this.setExpensesNegative(transaction)
+    );
+    // Az összes tranzakció sorbarendezése dátum szerint
+    this.sortTransactionsByDate();
 
-    // Aa teljes összeg kiszámítása
-    this.switchView(this.currentView); // Beállítja az összegz
+    //  Az összegzés kiszámítása a kiválasztott nézet szerint, azaz a kiadásokra állítja be az összegzést, amikor a komponens betöltődik (expenses a default)
+    this.switchView(this.currentView);
+  }
+
+  // Sorbarendezés dátum szerint, a tranzakciók tömbjében
+  sortTransactionsByDate() {
+    this.transactions.sort((a, b) => {
+      return (
+        new Date(a.transactionDate).getTime() -
+        new Date(b.transactionDate).getTime()
+      );
+    });
   }
 }
