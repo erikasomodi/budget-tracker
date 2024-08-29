964cad848ea3a7496c9cf43ee2fd8568ee0edd3f
Kázsmér Anita (Panni) <you@example.compannikazsmer@gmail.com>
2024-08-29 18:41:05 +0200

    navigate

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  selector: "app-home-page",
+  templateUrl: "./home-page.component.html",
+  styleUrl: "./home-page.component.scss",
 })
 export class HomePageComponent {
+  constructor(private router: Router) {}
+  navigateToLogin() {
+    this.router.navigate(["/login"]);
+  }
 
+  navigateToReg() {
+    this.router.navigate(["/registration"]);
+  }
 }
