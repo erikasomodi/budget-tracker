0780cb830807178cc140a01a65026db2eb5bb045
erikasomodi <meszesanerika@gmail.com>
2024-09-04 10:05:47 +0200

    transaction item ikonok

--- src/app/components/budget-tracker/transaction-item/transaction-item.component.ts
+++ src/app/components/budget-tracker/transaction-item/transaction-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from "@angular/core";
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import {
   faTrash,
   faMarker,
@@ -12,18 +12,18 @@ import {
   faUmbrellaBeach,
   faCar,
   faPlane,
-} from "@fortawesome/free-solid-svg-icons";
-import { TransactionModel } from "../../../models/transaction.model";
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { Router } from "@angular/router";
-import { UserService } from "../../../services/user.service";
-import { ToastrService } from "ngx-toastr";
-import { Subscription } from "rxjs";
+} from '@fortawesome/free-solid-svg-icons';
+import { TransactionModel } from '../../../models/transaction.model';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { Router } from '@angular/router';
+import { UserService } from '../../../services/user.service';
+import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: "app-transaction-item",
-  templateUrl: "./transaction-item.component.html",
-  styleUrls: ["./transaction-item.component.scss"],
+  selector: 'app-transaction-item',
+  templateUrl: './transaction-item.component.html',
+  styleUrls: ['./transaction-item.component.scss'],
 })
 export class TransactionItemComponent implements OnInit, OnDestroy {
   @Input() item!: TransactionModel;
@@ -39,8 +39,8 @@ export class TransactionItemComponent implements OnInit, OnDestroy {
   faChartLine = faChartLine;
   faCreditCard = faCreditCard;
   faMoneyBill = faMoneyBill;
-  // faCar = faCar;
-  // faPlane = faPlane;
+  faCar = faCar;
+  faPlane = faPlane;
 
   deleteSubscription?: Subscription;
 
@@ -53,21 +53,25 @@ export class TransactionItemComponent implements OnInit, OnDestroy {
 
   getIcon(): IconProp {
     switch (this.item.transactionCategory) {
-      case "shopping":
+      case 'shopping':
         return this.faTshirt;
-      case "recreation":
+      case 'recreation':
         return this.faUmbrellaBeach;
-      case "gifts":
+      case 'gifts':
         return this.faGift;
-      case "food":
+      case 'food':
         return this.faPizzaSlice;
-      case "electronic items":
+      case 'electronic items':
         return this.faLaptop;
-      case "investments":
+      case 'investments':
         return this.faChartLine;
-      case "income":
+      case 'travel':
+        return this.faCar;
+      case 'flight':
+        return this.faPlane;
+      case 'income':
         return this.faCreditCard;
-      case "freeLance":
+      case 'freeLance':
         return this.faMoneyBill;
       default:
         return this.icon;
@@ -79,20 +83,20 @@ export class TransactionItemComponent implements OnInit, OnDestroy {
         .removeTransactionFromUser(this.id!, transactionId)
         .subscribe({
           next: () => {
-            console.log("Tranzakció sikeresen törölve."),
-              this.toastr.success("Tranzakció sikeresen törölve");
-            this.router.navigate(["budget"]);
+            console.log('Tranzakció sikeresen törölve.'),
+              this.toastr.success('Tranzakció sikeresen törölve');
+            this.router.navigate(['budget']);
           },
           error: (error) => {
-            console.error("Hiba történt a tranzakció törlésekor:", error),
-              this.toastr.error("Hiba történt a tranzakció törlésekor");
+            console.error('Hiba történt a tranzakció törlésekor:', error),
+              this.toastr.error('Hiba történt a tranzakció törlésekor');
           },
         });
     }
   }
   transactionUpdate(id: number | undefined) {
     console.log(id);
-    this.router.navigate(["transaction-reg", id]);
+    this.router.navigate(['transaction-reg', id]);
   }
   ngOnDestroy(): void {
     if (this.deleteSubscription) {
