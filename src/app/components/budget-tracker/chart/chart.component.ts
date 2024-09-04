7fcd5c6903842cb4182e5b843ae1c342da27adc4
erikasomodi <meszesanerika@gmail.com>
2024-09-04 23:38:10 +0200

    responzív  design, diagram színezés, text__shadow sccsbe bevezetendő(Era)

--- src/app/components/budget-tracker/chart/chart.component.ts
+++ src/app/components/budget-tracker/chart/chart.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { TransactionModel } from '../../../models/transaction.model';
 import { Chart, registerables } from 'chart.js';
 import { UserModel } from '../../../models/user.model';
+import { ThemeService } from '../../../services/theme.service';
 
 @Component({
   selector: 'app-chart',
@@ -14,13 +15,16 @@ export class ChartComponent implements OnInit {
   @Input() transactions: TransactionModel[] = [];
   @Input() filteredTransactions: TransactionModel[] = [];
   @Input() startBudget?: UserModel['startBudget'];
-  constructor() {
+
+  chart?: Chart;
+
+  constructor(private themeService: ThemeService) {
     Chart.register(...registerables);
   }
 
   ngOnInit() {
     this.renderChart();
-    console.log(this.startBudget);
+    this.setupThemeChangeListener();
   }
 
   renderChart() {
@@ -37,7 +41,10 @@ export class ChartComponent implements OnInit {
       balance += transactionsWithStartBudget[i].transactionAmount ?? 0;
       balances.push(balance);
     }
-    new Chart(ctx, {
+
+    const darkMode = this.themeService.isDarkTheme();
+
+    this.chart = new Chart(ctx, {
       type: 'line',
       data: {
         labels: transactionsWithStartBudget.map((t) => t.transactionName),
@@ -45,8 +52,8 @@ export class ChartComponent implements OnInit {
           {
             label: 'Balance',
             data: balances,
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            borderColor: 'rgba(75, 192, 192, 1)',
+            backgroundColor: darkMode ? '#6edff6' : 'rgba(75, 192, 192, 0.2)',
+            borderColor: darkMode ? 'rgba(75, 192, 192, 1)' : '#087990',
             borderWidth: 1,
           },
         ],
@@ -55,9 +62,46 @@ export class ChartComponent implements OnInit {
         scales: {
           y: {
             beginAtZero: true,
+            ticks: {
+              color: darkMode ? 'white' : 'black',
+            },
+            grid: {
+              color: darkMode
+                ? 'rgba(255, 255, 255, 0.1)'
+                : 'rgba(0, 0, 0, 0.1)',
+            },
+          },
+          x: {
+            ticks: {
+              color: darkMode ? 'white' : 'black',
+            },
+            grid: {
+              color: darkMode
+                ? 'rgba(255, 255, 255, 0.1)'
+                : 'rgba(0, 0, 0, 0.1)',
+            },
+          },
+        },
+        plugins: {
+          legend: {
+            labels: {
+              color: darkMode ? 'white' : 'black',
+            },
           },
         },
       },
+    });
+  }
+
+  setupThemeChangeListener() {
+    const observer = new MutationObserver(() => {
+      this.chart?.destroy(); // Eltávolítja a régi diagramot
+      this.renderChart(); // Új diagram létrehozása az aktuális téma alapján
+    });
+
+    observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ['data-bs-theme'],
     });
   }
 }
